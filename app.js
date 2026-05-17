efdbfc3ff236d674d83dba38a66053c68a3d54d7
Rodrigo Vanegas <vanegasrodrigo18@hotmail.com>
2026-05-17 15:10:41 -0600

    New changes

--- app.js
+++ app.js
@@ -224,3 +224,42 @@ document.getElementById("recycleBtn")
 
 /* 🚀 INICIAR */
 loadDashboard();
+/* =========================
+   📢 ADS ROTATIVOS
+========================= */
+
+const adImage =
+  document.getElementById("adImage");
+
+const anuncios = [
+
+  "img/ad1.png",
+  "img/ad2.png",
+  "img/ad3.png"
+];
+
+let adActual = 0;
+
+function cambiarAd() {
+
+  if (!adImage) return;
+
+  adImage.style.opacity = 0;
+
+  setTimeout(() => {
+
+    adActual =
+      Math.floor(
+        Math.random() * anuncios.length
+      );
+
+    adImage.src =
+      anuncios[adActual];
+
+    adImage.style.opacity = 1;
+
+  }, 400);
+}
+
+/* 🔄 cambiar cada 5 segundos */
+setInterval(cambiarAd, 5000);
